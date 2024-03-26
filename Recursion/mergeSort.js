57f581b12c2091702b7106b31ab733d8ea0bcab4
Aaush Raj <rajaayush8340@gmail.com>
2024-03-26 16:41:14 +0530

    mergeSort with Array.shift() in recursion

--- Recursion/mergeSort.js
+++ Recursion/mergeSort.js
@@ -1,23 +1,4 @@
 
-
-// const mergeSortedArrays = (arr1,arr2)=>{
-
-//     const newArray = []
-//     for (let i = 0;i<arr1.length;i++){
-//         for (let j = 0;j<arr1.length;j++){
-//             if(arr1[i]<arr2[j]){
-//                 newArray.push(arr2[j]);
-//                 j++;
-//             }
-//             else{
-//                 newArray.push(arr1[i]);
-//                 i++
-//             }
-      
-//     }
-
-// }
-
 function mergeSort(arr) {
     if (arr.length <= 1) {
       return arr;
@@ -62,4 +43,37 @@ function mergeSort(arr) {
   
   console.log("Unsorted Array:", unsortedArray);
   console.log("Sorted Array:", sortedArray);
+  
+
+
+
+
+
+  // EASY IMPLEMENTATION OF MERGE SORT WITH SHIFT ARRAY FUNCTION, IT MAKES IT EASIER TO HANDLE THE MERGE PART OF SORTING
+
+  function mergeSort(arr ) {
+    if(arr.length <2)return arr;
+    const mid = Math.floor(arr.length / 2);
+    const leftArray = arr.slice(0,mid);
+    const rightArray = arr.slice(mid);
+  
+    return mergeSortedArrays(leftArray, rightArray);
+  }
+  
+  function mergeSortedArrays(leftArray, rightArray){
+  
+    let sortedArray = [];
+    while (leftArray.length && rightArray.length){
+      if (leftArray[0]<rightArray[0]){
+        sortedArray.push(leftArray.shift())
+      }
+      else{
+        sortedArray.push(rightArray.shift())
+      }
+    }
+    return [...sortedArray,...leftArray,...rightArray]
+  }
+  const arr = [12, 3, 4, 1, 32, 1, 2, 3, 3, 44, 90, 20];
+  
+  console.log(mergeSort(arr, 0, arr.length - 1));
   

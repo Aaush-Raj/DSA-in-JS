95eb11a79699564ab294997a129241642cdc631a
Aaush-Raj <rajaayush8340@gmail.com>
2024-04-21 12:41:07 +0530

    linked list

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -18,6 +18,23 @@ class LinkedList {
     this.size++;
   }
 
+  addAtIndex(newValue, index) {
+    if (index == 0) {
+      addFirst(newValue);
+    } else if (index == this.size) {
+      addLast(newValue);
+    } else {
+      let temp = this.head;
+      for (let i = 1; i < index; i++) {
+        temp = temp.next;
+      }
+
+      const newNode = new Node(newValue, temp.next);
+      temp.next = newNode;
+      this.size++;
+    }
+  }
+
   addLast(value) {
     const newNode = new Node(value);
     let currentNode = this.head;
@@ -28,6 +45,39 @@ class LinkedList {
     currentNode.next = newNode;
   }
 
+  deleteFirst() {
+    // let temp = this.head.next
+    // this.head = temp
+
+    //in one line
+    if (this.head !== null) {
+      this.head = this.head.next; // Move the head to the next node
+      this.size--; // Decrement size of the list
+    }
+  }
+
+  deleteLast() {
+    let temp = this.head;
+    // this.size-1 =  the seconds last index of LL
+    for (let i = 0; i < this.size - 1; i++) {
+      temp = temp.next;
+    }
+    temp.next = null;
+  }
+
+
+  deleteAtIndex(index) {
+    if (index == 0) {
+      this.deleteFirst();
+    }
+
+    let temp = this.head;
+    for (let i = 1; i < index; i++) {
+      temp = temp.next;
+    }
+    temp.next = temp.next.next;
+    this.size--;
+  }
   printList() {
     let current = this.head;
     while (current !== null) {
@@ -44,4 +94,3 @@ list.addFirst(300);
 list.addLast(10202);
 
 list.printList();
-
